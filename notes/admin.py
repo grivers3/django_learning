0941c14f81eb9e82407429073b9f8aa2f85fb807
grivers3 <twoliveoak@yahoo.com>
2022-03-21 13:30:35 +0000

    Added alternative

--- notes/admin.py
+++ notes/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 from . import models
-
+#from .models import Notes
 
 class NotesAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
 
 admin.site.register(models.Notes, NotesAdmin)
+#admin.site.register(Notes, NotesAdmin)
